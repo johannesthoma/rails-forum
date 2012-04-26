85c3a8fc9816f108f661591363dda921d9b906bf
Johannes Thoma <johannes.thoma@gmx.at>
2012-04-26 18:27:20 +0200

    Inital checking - scratch rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsForum::Application.config.secret_token = '3c98c0c5b90f739ba8b935e6459f135306d9ed89e5cf365cfcc85ec03a771131bea6d128ddd0c4c28c47294fb07c7cd4c67a3e0f04eb8c63d0d20c6c758355ca'
